c561769136ea95c3c41b441f3e035309d499a123
Shadow <shadowguoguo@gmail.com>
2025-11-08 19:37:09 +0800

    re-constructered INTERFACE

--- COMP2021_Project/COMP2021/src/COMP2021_Project/INTERFACE.java
+++ COMP2021_Project/COMP2021/src/COMP2021_Project/INTERFACE.java
@@ -13,90 +13,99 @@ public class INTERFACE {
      * @return
      */
     public static GUIInterface getGui(){return gui;}
+
     /**
      * @param args parameter inputs when run the program
      */
     public static void main(String[] args) {
-        if(!(LOGS.IshtmlPathEffective(args)&&LOGS.IstxtPathEffective(args))){
+        if(!(filePathTest(args))){
             System.out.println("Please try again!");
             gui.updateOutput("Please try again!");
             return;
         }
         while(true){
             String sc = scanner.nextLine();
-            gui.DrawShape(SHAPE.AllShapes);
-            String[] inputs = sc.trim().split(" ");
-            gui.updateOutput(sc);
-            int n=inputs.length;
-            String command=inputs[0].toLowerCase();
-            IS_EFFECTIVE sign=new IS_EFFECTIVE(inputs,command);
-            if(!sign.IsEffective()){
-                System.out.println("Please try again!");
-                gui.updateOutput("Please try again!");
-                continue;
-            }
-            new LOGS(inputs);
-            double x,y;
-            switch (command){
-                case "rectangle":
-                case "circle":
-                case "line":
-                case "square":
-                    double[] arg=new double[n-2];
-                    for(int i=0;i<arg.length;i++){
-                        arg[i]=Double.parseDouble(inputs[2+i]);
-                    }
-                    new CREATE(command,inputs[1],arg);
-                    break;
-                case "group":
-                    String[] members=new String[n-2];
-                    System.arraycopy(inputs, 2, members, 0, members.length);
-                    new _GROUP(inputs[1],members);
-                    break;
-                case "ungroup":
-                    new UNGROUP(inputs[1]);
-                    break;
-                case "delete":
-                    new DELETE(inputs[1]);
-                    break;
-                case "move":
-                    x=Double.parseDouble(inputs[2]);
-                    y=Double.parseDouble(inputs[3]);
-                    new MOVE(inputs[1],x,y);
-                    break;
-                case "boundingbox":
-                    double[] box=QUERY.boundingBox(OPERATION.get(inputs[1]));
-                    String out = String.format("%.2f %.2f %.2f %.2f\n",box[0],box[1],box[2],box[3]);
-                    System.out.printf(out);
-                    gui.updateOutput(out);
-                    break;
-                case "shapeat":
-                    x=Double.parseDouble(inputs[1]);
-                    y=Double.parseDouble(inputs[2]);
-                    System.out.println(QUERY.shapeAt(x,y));
-                    break;
-                case "intersect":
-                    System.out.println(QUERY.insection(OPERATION.get(inputs[1]),OPERATION.get(inputs[2])));
-                    break;
-                case "list":
-                    QUERY.list(OPERATION.get(inputs[1]),"");
-                    break;
-                case "listall":
-                    QUERY.listall();
-                    break;
-                case "undo":
-                    OPERATION.UNDO();
-                    break;
-                case "redo":
-                    OPERATION.REDO();
-                    break;
-                case "quit":
-                    System.out.println("Goodbye!");
-                    gui.updateOutput("Goodbye!");
-                    /**@lqc: 在这里关闭并保存日志*/
-                    LOGS.CloseFile();
-                    System.exit(0);
-            }
+            commandProcess(sc);
         }
+    }
+
+    public static boolean filePathTest(String[] args){
+        return LOGS.IshtmlPathEffective(args)&&LOGS.IstxtPathEffective(args);
+    }
+    public static void commandProcess(String sc){
+        gui.updateOutput(sc);
+        String[] inputs = sc.trim().split(" ");
+        int n=inputs.length;
+        String command=inputs[0].toLowerCase();
+        IS_EFFECTIVE sign=new IS_EFFECTIVE(inputs,command);
+        if(!sign.IsEffective()){
+            System.out.println("Please try again!");
+            gui.updateOutput("Please try again!");
+            return;
+        }
+        new LOGS(inputs);
+        double x,y;
+        switch (command){
+            case "rectangle":
+            case "circle":
+            case "line":
+            case "square":
+                double[] arg=new double[n-2];
+                for(int i=0;i<arg.length;i++){
+                    arg[i]=Double.parseDouble(inputs[2+i]);
+                }
+                new CREATE(command,inputs[1],arg);
+                break;
+            case "group":
+                String[] members=new String[n-2];
+                System.arraycopy(inputs, 2, members, 0, members.length);
+                new _GROUP(inputs[1],members);
+                break;
+            case "ungroup":
+                new UNGROUP(inputs[1]);
+                break;
+            case "delete":
+                new DELETE(inputs[1]);
+                break;
+            case "move":
+                x=Double.parseDouble(inputs[2]);
+                y=Double.parseDouble(inputs[3]);
+                new MOVE(inputs[1],x,y);
+                break;
+            case "boundingbox":
+                double[] box=QUERY.boundingBox(OPERATION.get(inputs[1]));
+                String out = String.format("%.2f %.2f %.2f %.2f\n",box[0],box[1],box[2],box[3]);
+                System.out.printf(out);
+                gui.updateOutput(out);
+                break;
+            case "shapeat":
+                x=Double.parseDouble(inputs[1]);
+                y=Double.parseDouble(inputs[2]);
+                System.out.println(QUERY.shapeAt(x,y));
+                break;
+            case "intersect":
+                System.out.println(QUERY.insection(OPERATION.get(inputs[1]),OPERATION.get(inputs[2])));
+                break;
+            case "list":
+                QUERY.list(OPERATION.get(inputs[1]),"");
+                break;
+            case "listall":
+                QUERY.listall();
+                break;
+            case "undo":
+                OPERATION.UNDO();
+                break;
+            case "redo":
+                OPERATION.REDO();
+                break;
+            case "quit":
+                System.out.println("Goodbye!");
+                gui.updateOutput("Goodbye!");
+                /**@lqc: 在这里关闭并保存日志*/
+                LOGS.CloseFile();
+                System.exit(0);
+        }
+        gui.DrawShape(SHAPE.AllShapes);
+
     }
 }
--- COMP2021_Project/COMP2021/src/COMP2021_Project/INTERFACETest.java
+++ COMP2021_Project/COMP2021/src/COMP2021_Project/INTERFACETest.java
@@ -8,35 +8,35 @@ import static org.junit.Assert.assertTrue;
  */
 public class INTERFACETest {
     @Test
-    public void test(){
-        if(LOGS.IshtmlPathEffective(new String[]{System.getProperty("user.dir"),System.getProperty("user.dir")})){}
+    public void testCoverage(){
+        INTERFACE.filePathTest(new String[]{System.getProperty("user.dir"),System.getProperty("user.dir")});
 
-        INTERFACE.deal("rectangle rect1 10 10 50 30");
+        INTERFACE.commandProcess("rectangle rect1 10 10 50 30");
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        INTERFACE.deal("line line1 5 5 45 25");
-        INTERFACE.deal("circle circle1 30 30 15");
-        INTERFACE.deal("square square1 60 60 25");
-        INTERFACE.deal("rectangle rect2 80 20 40 40");
-        INTERFACE.deal("list rect1");
-        INTERFACE.deal("listAll");
-        INTERFACE.deal("boundingbox rect1");
-        INTERFACE.deal("shapeAt 15 15");
-        INTERFACE.deal("intersect rect1 circle1");
-        INTERFACE.deal("move rect1 5 -5");
-        INTERFACE.deal("move circle1 -10 10");
-        INTERFACE.deal("group group1 rect1 circle1");
-        INTERFACE.deal("list group1");
-        INTERFACE.deal("move group1 20 -20");
-        INTERFACE.deal("boundingbox group1");
-        INTERFACE.deal("intersect group1 rect2");
-        INTERFACE.deal("ungroup group1");
-        INTERFACE.deal("move rect1 -5 5");
-//        INTERFACE.deal("");
-//        INTERFACE.deal("quit");
+        INTERFACE.commandProcess("line line1 5 5 45 25");
+        INTERFACE.commandProcess("circle circle1 30 30 15");
+        INTERFACE.commandProcess("square square1 60 60 25");
+        INTERFACE.commandProcess("rectangle rect2 80 20 40 40");
+        INTERFACE.commandProcess("list rect1");
+        INTERFACE.commandProcess("listAll");
+        INTERFACE.commandProcess("boundingbox rect1");
+        INTERFACE.commandProcess("shapeAt 15 15");
+        INTERFACE.commandProcess("intersect rect1 circle1");
+        INTERFACE.commandProcess("move rect1 5 -5");
+        INTERFACE.commandProcess("move circle1 -10 10");
+        INTERFACE.commandProcess("group group1 rect1 circle1");
+        INTERFACE.commandProcess("list group1");
+        INTERFACE.commandProcess("move group1 20 -20");
+        INTERFACE.commandProcess("boundingbox group1");
+        INTERFACE.commandProcess("intersect group1 rect2");
+        INTERFACE.commandProcess("ungroup group1");
+        INTERFACE.commandProcess("move rect1 -5 5");
+//        INTERFACE.commandProcess("");
+//        INTERFACE.commandProcess("quit");
         assertTrue(true);
     }
 }
